8de89aee9ad365fe41354c31eb3d90b753b983b9
Bhimraj Ghadge <bhimraj@infysion.com>
2019-06-27 15:07:46 +0530

    #2222 - For GetCart API call - allow API Json request to return HTML as HTML not JSON.

--- Assemblies/ProteanCMS/CMS/Cart/Cart.JSONActions.vb
+++ Assemblies/ProteanCMS/CMS/Cart/Cart.JSONActions.vb
@@ -57,17 +57,7 @@ Partial Public Class Cms
                 End If
                 CartXml.FirstChild.AppendChild(cartItems)
 
-                'Update the XML to wrap up HTML tags in the CDATA section.
-                Dim outputXml As String = CartXml.OuterXml
-                Dim tagList = New List(Of String) From {"p", "div", "ul"}
-                For Each tag As String In tagList
-                    outputXml = outputXml.Replace($"<{tag}>", $"<![CDATA[<{tag}>")
-                    outputXml = outputXml.Replace($"</{tag}>", $"</{tag}>]]>")
-                Next
-
-                Dim xdoc As XmlDocument = New XmlDocument
-                xdoc.LoadXml(outputXml)
-                CartXml = xdoc.FirstChild
+                TidyHtmltoCData(CartXml)
 
                 Return CartXml
             End Function
