9465b7bded5b8cae1cf33545119f2412c3069ab0
trevski <trevor@eonic.co.uk>
2019-10-14 10:51:59 +0100

    Updates

--- Assemblies/Protean.Tools/AssemblyInfo.vb
+++ Assemblies/Protean.Tools/AssemblyInfo.vb
@@ -33,8 +33,8 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.0.23.172")>
-<Assembly: AssemblyFileVersion("6.0.23.172")>
+<Assembly: AssemblyVersion("6.0.23.175")>
+<Assembly: AssemblyFileVersion("6.0.23.175")>
 
 '<Assembly: System.Security.AllowPartiallyTrustedCallersAttribute()> 
 
---
+++ Assemblies/ProteanCMS.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProteanCMS.IntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProteanCMS.IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("58ca7ed9-d49f-42dc-83ea-04a4a5bcbb28")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
