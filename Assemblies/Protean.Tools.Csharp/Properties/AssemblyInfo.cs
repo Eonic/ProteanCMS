9b7d10e7d9b9354f5324ca388547a7d84198decb
Luke Thompson <luket@office.eonic.co.uk>
2019-10-24 11:53:36 +0100

    Fix for ECMA not installed

--- Assemblies/Protean.Tools.Csharp/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools.Csharp/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.0.23.144")]
-[assembly: AssemblyFileVersion("6.0.23.144")]
+[assembly: AssemblyVersion("6.0.24.0")]
+[assembly: AssemblyFileVersion("6.0.24.0")]
--- Assemblies/Protean.Tools/AssemblyInfo.vb
+++ Assemblies/Protean.Tools/AssemblyInfo.vb
@@ -33,8 +33,8 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.0.23.169")>
-<Assembly: AssemblyFileVersion("6.0.23.169")>
+<Assembly: AssemblyVersion("6.0.24.0")>
+<Assembly: AssemblyFileVersion("6.0.24.0")>
 
 '<Assembly: System.Security.AllowPartiallyTrustedCallersAttribute()> 
 
