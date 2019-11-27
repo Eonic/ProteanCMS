5d93ef7f4a89a36dba19d4c3c9bf30104817ad8d
Luke Thompson <luket@office.eonic.co.uk>
2019-11-27 15:02:42 +0000

    Magick.NET package

--- Assemblies/Protean.Tools/AssemblyInfo.vb
+++ Assemblies/Protean.Tools/AssemblyInfo.vb
@@ -33,8 +33,8 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.0.33.26")>
-<Assembly: AssemblyFileVersion("6.0.33.26")>
+<Assembly: AssemblyVersion("6.0.33.31")>
+<Assembly: AssemblyFileVersion("6.0.33.31")>
 
 '<Assembly: System.Security.AllowPartiallyTrustedCallersAttribute()> 
 
