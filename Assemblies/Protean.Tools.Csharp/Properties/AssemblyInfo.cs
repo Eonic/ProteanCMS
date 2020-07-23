2351966069bd5127ab21bcf4a4976a07f5043672
Trevor Spink <trevors@office.eonic.co.uk>
2020-07-23 13:20:46 +0100

    Latest release

--- Assemblies/Protean.Tools.Csharp/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools.Csharp/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.0.35.50")]
-[assembly: AssemblyFileVersion("6.0.35.49")]
+[assembly: AssemblyVersion("6.0.37.1")]
+[assembly: AssemblyFileVersion("6.0.37.1")]
--- Assemblies/ProteanCMS/AssemblyInfo.vb
+++ Assemblies/ProteanCMS/AssemblyInfo.vb
@@ -33,11 +33,11 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("6.0.35.511")>
+<Assembly: AssemblyVersion("6.0.37.0")>
 <Assembly: AssemblyDelaySign(False)>
 <Assembly: AssemblyKeyFile("../../../eonic.snk")>
 <Assembly: AssemblyKeyName("")>
 
-<Assembly: AssemblyFileVersion("6.0.35.509")>
+<Assembly: AssemblyFileVersion("6.0.37.0")>
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
 
