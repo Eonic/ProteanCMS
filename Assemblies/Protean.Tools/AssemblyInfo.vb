8436b3a0c8e1dfdff9647b98abce30703fddcd36
Trevor Spink <trevors@office.eonic.co.uk>
2020-09-10 13:24:53 +0100

    Release to 6.0.39

--- Assemblies/Protean.Tools/AssemblyInfo.vb
+++ Assemblies/Protean.Tools/AssemblyInfo.vb
@@ -33,8 +33,8 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.0.38.29")>
-<Assembly: AssemblyFileVersion("6.0.38.29")>
+<Assembly: AssemblyVersion("6.0.39.0")>
+<Assembly: AssemblyFileVersion("6.0.39.0")>
 
 '<Assembly: System.Security.AllowPartiallyTrustedCallersAttribute()> 
 
--- Assemblies/ProteanCMS.Installer.Actions/CustomAction.vb
+++ Assemblies/ProteanCMS.Installer.Actions/CustomAction.vb
@@ -10,7 +10,7 @@ Imports System.windows.forms
 
 Public Class CustomActions
 
-    Public Shared ewAssemblyVersion As String = "6.0.38.0"
+    Public Shared ewAssemblyVersion As String = "6.0.39.0"
     Public Shared ptnAppStartAssemblyVersion As String = "6.0.0.0"
     Public Shared bundleAssemblyVersion As String = "1.10.0.0"
     Public Shared bundleLessAssemblyVersion As String = "1.10.4.0"
@@ -33,7 +33,8 @@ Public Class CustomActions
     Public Shared TidyHTML5ManagedAssemblyVersion As String = "1.1.5.0"
     Public Shared ClearScriptAssemblyVersion As String = "5.5.6.0"
     Public Shared AlphaFSAssemblyVersion As String = "2.2.0.0"
-    Public Shared MagickNETAssemblyVersion As String = "7.15.1.0"
+    Public Shared MagickNETAssemblyVersion As String = "7.21.1.0"
+    Public Shared MagickNETCoreAssemblyVersion As String = "4.1.0.0"
 
     <CustomAction()> _
     Public Shared Function LoadGuide(ByVal session As Session) As ActionResult
@@ -663,8 +664,18 @@ Public Class CustomActions
                 BindingElmt.AppendChild(newElmt)
             End If
             For Each oElmt In oSectXml.SelectNodes("/runtime/assemblyBinding/dependentAssembly[assemblyIdentity/@name='Magick.NET-Q8-AnyCPU']/bindingRedirect")
-                oElmt.SetAttribute("oldVersion", "0.0.0.0-7.0.0.0")
+                oElmt.SetAttribute("oldVersion", "0.0.0.0-" & MagickNETAssemblyVersion)
                 oElmt.SetAttribute("newVersion", MagickNETAssemblyVersion)
+            Next
+
+            If oSectXml.SelectSingleNode("/runtime/assemblyBinding/dependentAssembly[assemblyIdentity/@name='Magick.NET.Core']") Is Nothing Then
+                Dim newElmt As XmlElement = oSectXml.CreateElement("dependentAssembly")
+                newElmt.InnerXml = "<assemblyIdentity name=""Magick.NET.Core"" publicKeyToken=""2004825badfa91ec""/><bindingRedirect/>"
+                BindingElmt.AppendChild(newElmt)
+            End If
+            For Each oElmt In oSectXml.SelectNodes("/runtime/assemblyBinding/dependentAssembly[assemblyIdentity/@name='Magick.NET.Core']/bindingRedirect")
+                oElmt.SetAttribute("oldVersion", "0.0.0.0-" & MagickNETCoreAssemblyVersion)
+                oElmt.SetAttribute("newVersion", MagickNETCoreAssemblyVersion)
             Next
 
 
