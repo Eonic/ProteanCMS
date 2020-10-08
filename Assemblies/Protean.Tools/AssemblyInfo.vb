b558c7d3bd9c0e0114d916770dac6615a17f9115
Trevor Spink <trevors@office.eonic.co.uk>
2020-10-08 09:59:02 +0100

    merge

--- Assemblies/Protean.Tools/AssemblyInfo.vb
+++ Assemblies/Protean.Tools/AssemblyInfo.vb
@@ -33,8 +33,8 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.0.42.36")>
-<Assembly: AssemblyFileVersion("6.0.42.36")>
+<Assembly: AssemblyVersion("6.0.42.38")>
+<Assembly: AssemblyFileVersion("6.0.42.38")>
 
 '<Assembly: System.Security.AllowPartiallyTrustedCallersAttribute()> 
 
--- Assemblies/ProteanCMS/AssemblyInfo.vb
+++ Assemblies/ProteanCMS/AssemblyInfo.vb
@@ -33,11 +33,11 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("6.0.42.101")>
+<Assembly: AssemblyVersion("6.0.42.103")>
 <Assembly: AssemblyDelaySign(False)>
 <Assembly: AssemblyKeyFile("../../../eonic.snk")>
 <Assembly: AssemblyKeyName("")>
 
-<Assembly: AssemblyFileVersion("6.0.42.99")>
+<Assembly: AssemblyFileVersion("6.0.42.101")>
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
 
