2dde6b63ef78dbc74bc1bdffe3a08ef7e86b741c
Trevor Spink <trevors@office.eonic.co.uk>
2020-11-17 13:02:45 +0000

    Updates to add in csharp tools

---
+++ Assemblies/ProteanCMS/CMS/Admin/Admin.JSONActions.vb
@@ -0,0 +1,76 @@
+﻿Option Strict Off
+Option Explicit On
+Imports System.Xml
+Imports System.Collections
+Imports System.Web.Configuration
+Imports System.Data.SqlClient
+Imports System.Web.HttpUtility
+Imports VB = Microsoft.VisualBasic
+Imports System.IO
+Imports Protean.Tools.Xml
+Imports Protean.Tools.Xml.XmlNodeState
+Imports System
+Imports TweetSharp
+Imports System.Collections.Generic
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+Partial Public Class Cms
+
+    Partial Public Class Admin
+
+#Region "JSON Actions"
+
+        Public Class JSONActions
+            Public Event OnError(ByVal sender As Object, ByVal e As Protean.Tools.Errors.ErrorEventArgs)
+            Private Const mcModuleName As String = "Eonic.Content.JSONActions"
+            Private moLmsConfig As System.Collections.Specialized.NameValueCollection = WebConfigurationManager.GetWebApplicationSection("protean/lms")
+            Private myWeb As Protean.Cms
+            Private myCart As Protean.Cms.Cart
+
+            Public Sub New()
+                Dim ctest As String = "this constructor is being hit" 'for testing
+                myWeb = New Protean.Cms()
+                myWeb.InitializeVariables()
+                myWeb.Open()
+                myCart = New Protean.Cms.Cart(myWeb)
+            End Sub
+
+            Public Function CheckRedirects(ByRef myApi As Protean.API, ByRef jObj As Newtonsoft.Json.Linq.JObject) As String
+                Try
+
+
+                    Dim PageId As Long
+                    Dim ArticleId As Long
+                    Dim NewPageName As String
+
+
+                    Dim OldURL As String
+                    Dim NewURL As String
+
+
+
+                    'Validate that the user is an administrator with session user ID
+
+                    'Check we do not have a redirect for the OLD URL allready.
+
+                    'Determine all the paths that need to be redirected
+
+
+                    Dim JsonResult As String = ""
+                    Return JsonResult
+                Catch ex As Exception
+                    RaiseEvent OnError(Me, New Protean.Tools.Errors.ErrorEventArgs(mcModuleName, "GetCart", ex, ""))
+                    Return ex.Message
+                End Try
+            End Function
+
+
+        End Class
+
+#End Region
+
+    End Class
+
+End Class
+
