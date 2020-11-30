fa71f6a55c68264f9f77275ba0f0d875c83627ce
Trevor Spink <trevors@office.eonic.co.uk>
2020-11-30 09:28:19 +0000

    Updates for discount rule saving

--- Assemblies/Protean.Tools/AssemblyInfo.vb
+++ Assemblies/Protean.Tools/AssemblyInfo.vb
@@ -33,8 +33,8 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.0.44.15")>
-<Assembly: AssemblyFileVersion("6.0.44.15")>
+<Assembly: AssemblyVersion("6.0.44.21")>
+<Assembly: AssemblyFileVersion("6.0.44.21")>
 
 '<Assembly: System.Security.AllowPartiallyTrustedCallersAttribute()> 
 
