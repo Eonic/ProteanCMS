98530f6c67f29f86320af9f66fde24b6d6464848
Bhimraj Ghadge <bhimraj@infysion.com>
2020-12-08 16:23:53 +0530

    #4027 - some more updates to audit fix script for duplicate records. For duplicates, need to copy status of existing audit record so that it will not change status after fixes.

--- wwwroot/ewcommon/setup/testscripts/AuditId_Fix_tblCartItems.sql
+++ wwwroot/ewcommon/setup/testscripts/AuditId_Fix_tblCartItems.sql
@@ -2,7 +2,8 @@
 	DECLARE @CartItemWIthInvalidAuditIds TABLE
 	(
 		nCartItemKey INT,
-		nAuditId INT
+		nAuditId INT,
+		isDuplicate BIT
 	)
 
 ---------------------------------------------------------------------------------------
@@ -23,14 +24,14 @@
 	FROM 
 	(
 		--No AuditId
-		SELECT nCartItemKey, nAuditId
+		SELECT nCartItemKey, nAuditId, 0 AS isDuplicate
 		FROM tblCartItem C
 		WHERE nAuditId IS NULL OR nAuditId = 0
 
 		UNION
 
 		--Invalid AuditId's. Does not exist in the tblAudit table.
-		SELECT nCartItemKey, nAuditId
+		SELECT nCartItemKey, nAuditId, 0 AS isDuplicate
 		FROM tblCartItem C
 		WHERE NOT EXISTS
 		(
@@ -41,7 +42,7 @@
 
 		UNION
 
-		SELECT nCartItemKey, nAuditId
+		SELECT nCartItemKey, nAuditId, 1 AS isDuplicate
 		FROM TEMP
 		WHERE Row_Num > 1
 	) C
@@ -56,7 +57,10 @@
 --Fix AuditIds
 
 	DECLARE @nCartItemKey INT
-	DECLARE @AuditId INT
+	DECLARE @originalAuditId INT
+	DECLARE @newAuditId INT
+	DECLARE @isDuplicate BIT
+	DECLARE @auditStatus BIT
 
     BEGIN TRY
         BEGIN TRAN
@@ -68,18 +72,29 @@
 
 			FOR
 				SELECT
-					nCartItemKey
+					nCartItemKey,
+					nAuditId,
+					isDuplicate
 				FROM @CartItemWIthInvalidAuditIds
 
 			OPEN Cur
 			FETCH NEXT FROM Cur INTO
-			@nCartItemKey
+			@nCartItemKey, @originalAuditId, @isDuplicate
 			-- Assigns values to variables declared at the top
 
 
 			WHILE @@FETCH_STATUS = 0
 			BEGIN
-				SET @AuditId = 0
+				SET @newAuditId = 0
+				SET @auditStatus = 0 -- Default Audit status would be Hidden
+
+				--For duplicate records - set the status of new audit record to what it was originally.
+				IF @isDuplicate = 1
+				BEGIN
+					SELECT @auditStatus = nStatus
+					FROM tblAudit
+					WHERE nAuditKey = @originalAuditId
+				END
 	
 				-- Inserts the Audit record 
 				INSERT INTO tblAudit 
@@ -90,14 +105,14 @@
 					0 AS nInsertDirId, 
 					GETDATE() AS dUpdateDate, 
 					0 AS nUpdateDirId, 
-					0 AS nStatus, 
+					@auditStatus AS nStatus, 
 					NULL AS cDescription
 
-				SET @AuditId = SCOPE_IDENTITY()
+				SET @newAuditId = SCOPE_IDENTITY()
 
 
 				UPDATE tblCartItem
-				SET nAuditId = @AuditId
+				SET nAuditId = @newAuditId
 				WHERE nCartItemKey = @nCartItemKey
 
 				FETCH NEXT FROM Cur INTO @nCartItemKey
--- wwwroot/ewcommon/setup/testscripts/AuditId_Fix_tblContent.sql
+++ wwwroot/ewcommon/setup/testscripts/AuditId_Fix_tblContent.sql
@@ -2,7 +2,8 @@
 	DECLARE @ContentWIthInvalidAuditIds TABLE
 	(
 		nContentKey INT,
-		nAuditId INT
+		nAuditId INT,
+		isDuplicate BIT
 	)
 
 ---------------------------------------------------------------------------------------
@@ -23,14 +24,14 @@
 	FROM 
 	(
 		--No AuditId
-		SELECT nContentKey, nAuditId
+		SELECT nContentKey, nAuditId, 0 AS isDuplicate
 		FROM tblContent c 
 		WHERE nAuditId IS NULL OR nAuditId = 0
 
 		UNION
 
 		--Invalid AuditId's. Does not exist in the tblAudit table.
-		SELECT nContentKey, nAuditId
+		SELECT nContentKey, nAuditId, 0 AS isDuplicate
 		FROM tblContent C
 		WHERE NOT EXISTS
 		(
@@ -41,7 +42,7 @@
 
 		UNION
 
-		SELECT nContentKey, nAuditId
+		SELECT nContentKey, nAuditId, 1 AS isDuplicate
 		FROM TEMP
 		WHERE Row_Num > 1
 	) C
@@ -56,7 +57,10 @@
 --Fix AuditIds
 
 	DECLARE @nContentKey INT
-	DECLARE @AuditId INT
+	DECLARE @originalAuditId INT
+	DECLARE @newAuditId INT
+	DECLARE @isDuplicate BIT
+	DECLARE @auditStatus BIT
 
     BEGIN TRY
         BEGIN TRAN
@@ -68,18 +72,29 @@
 
 			FOR
 				SELECT
-					nContentKey
+					nContentKey,
+					nAuditId,
+					isDuplicate
 				FROM @ContentWIthInvalidAuditIds
 
 			OPEN Cur
 			FETCH NEXT FROM Cur INTO
-			@nContentKey
+			@nContentKey, @originalAuditId, @isDuplicate
 			-- Assigns values to variables declared at the top
 
 
 			WHILE @@FETCH_STATUS = 0
 			BEGIN
-				SET @AuditId = 0
+				SET @newAuditId = 0
+				SET @auditStatus = 0 -- Default Audit status would be Hidden
+
+				--For duplicate records - set the status of new audit record to what it was originally.
+				IF @isDuplicate = 1
+				BEGIN
+					SELECT @auditStatus = nStatus
+					FROM tblAudit
+					WHERE nAuditKey = @originalAuditId
+				END
 	
 				-- Inserts the Audit record 
 				INSERT INTO tblAudit 
@@ -90,14 +105,14 @@
 					0 AS nInsertDirId, 
 					GETDATE() AS dUpdateDate, 
 					0 AS nUpdateDirId, 
-					0 AS nStatus, 
+					@auditStatus AS nStatus, 
 					NULL AS cDescription
 
-				SET @AuditId = SCOPE_IDENTITY()
+				SET @newAuditId = SCOPE_IDENTITY()
 
 
 				UPDATE tblContent
-				SET nAuditId = @AuditId
+				SET nAuditId = @newAuditId
 				WHERE nContentKey = @nContentKey
 
 				FETCH NEXT FROM Cur INTO @nContentKey
