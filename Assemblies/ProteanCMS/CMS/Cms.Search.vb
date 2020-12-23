7e02f92325881d84199dd5df1a96841a12912b31
Sonali Sonwane <sonali.sonwane@infysion.com>
2020-12-23 12:46:20 +0530

    #5126: search index pagination

--- Assemblies/ProteanCMS/CMS/Cms.Search.vb
+++ Assemblies/ProteanCMS/CMS/Cms.Search.vb
@@ -739,7 +739,7 @@ Partial Public Class Cms
 
 
                 resultsXML.SetAttribute("SearchString", cQuery)
-                    resultsXML.SetAttribute("searchType", "INDEX")
+                resultsXML.SetAttribute("searchType", "INDEX")
                 resultsXML.SetAttribute("type", "SearchHeader")
 
 
