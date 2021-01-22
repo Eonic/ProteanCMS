f4c103e007875e1f1b8ad78e857157e2b542633f
Trevor Spink <trevors@office.eonic.co.uk>
2021-01-22 16:46:31 +0000

    build

--- Assemblies/Protean.Tools/AssemblyInfo.vb
+++ Assemblies/Protean.Tools/AssemblyInfo.vb
@@ -33,8 +33,8 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.0.44.65")>
-<Assembly: AssemblyFileVersion("6.0.44.65")>
+<Assembly: AssemblyVersion("6.0.44.66")>
+<Assembly: AssemblyFileVersion("6.0.44.66")>
 
 '<Assembly: System.Security.AllowPartiallyTrustedCallersAttribute()> 
 
