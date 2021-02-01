075d6b16119a487b02257da5300716f9cee0d098
Trevor Spink <trevors@office.eonic.co.uk>
2021-02-01 15:22:58 +0000

    fix

--- wwwroot/ewcommon/js/ewAdmin.js
+++ wwwroot/ewcommon/js/ewAdmin.js
@@ -2010,7 +2010,7 @@ function formatXml2(xml) {
 }
 
 function checkTextAreaMaxLength(textBoxRef, e) {
-    var myTextBox = $("input#" + textBoxRef);
+    var myTextBox = $("#" + textBoxRef);
     var maxLength = parseInt(myTextBox.data("length"));
 
     if (!checkSpecialKeys(e)) {
