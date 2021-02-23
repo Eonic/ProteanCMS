b0491311c29501531d68e9b31aab2c03713fa6fb
Trevor Spink <trevors@office.eonic.co.uk>
2021-02-23 16:05:47 +0000

    Update

--- Assemblies/ProteanCMS/AssemblyInfo.vb
+++ Assemblies/ProteanCMS/AssemblyInfo.vb
@@ -33,11 +33,11 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("6.0.45.4")>
+<Assembly: AssemblyVersion("6.0.45.5")>
 <Assembly: AssemblyDelaySign(False)>
 <Assembly: AssemblyKeyFile("../../../eonic.snk")>
 <Assembly: AssemblyKeyName("")>
 
-<Assembly: AssemblyFileVersion("6.0.45.4")>
+<Assembly: AssemblyFileVersion("6.0.45.5")>
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
 
