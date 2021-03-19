1e9c1aface93c4837727d1fbf1a84e59a64c7673
Sonali Sonwane <sonali.sonwane@infysion.com>
2021-03-19 20:30:56 +0530

    #5404: page/product redirect changes.

--- wwwroot/ewcommon/js/vuejs/protean-vue.js
+++ wwwroot/ewcommon/js/vuejs/protean-vue.js
@@ -658,9 +658,21 @@ $('.scolling-pane').on('scroll', function () {
             }
         }
 
-    }
-});
+            },
+        },
+        mounted: function () {
+            this.storedPath();
+        }
+    });
+}
 
+$(document).on("click", ".btnSaveProduct", function (event) {
+
+    var newContentPath = $("#cContentPath").val();
+    editProduct.UrlPathOnChange(newContentPath);
+
+
+});
 
 //Insights
 const insightsSectionElement = document.querySelector("#insights-section");
