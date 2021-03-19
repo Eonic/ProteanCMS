199671dc9279b9c21b2ede50688a15932e02ebe4
Sonali Sonwane <sonali.sonwane@infysion.com>
2021-03-19 13:41:01 +0530

    #5404: page/product redirect changes.

--- Assemblies/ProteanCMS/CMS/Admin/AdminXforms.vb
+++ Assemblies/ProteanCMS/CMS/Admin/AdminXforms.vb
@@ -2852,6 +2852,7 @@ Partial Public Class Cms
                         End If
                     End If
 
+                    myWeb.GetContentXml(MyBase.Instance)
 
 
                     If MyBase.isSubmitted Then
@@ -2905,7 +2906,10 @@ Partial Public Class Cms
                                 Dim redirectType As String = ""
                                 Dim strNewUrl As String = ""
                                 Dim strOldurl As String = ""
-                                Dim oAdminRedirect As Admin.Redirects = New Admin.Redirects()
+                                Dim oURL As String = ""
+                                If moRequest("redirectType") IsNot Nothing Then
+                                    redirectType = moRequest("redirectType").ToString()
+                                End If
 
                                 If moRequest("productNewUrl") IsNot Nothing Then
                                     strNewUrl = moRequest("productNewUrl").ToString()
@@ -2923,6 +2927,16 @@ Partial Public Class Cms
                                 If myWeb.moConfig("RewriteRuleForProduct") IsNot Nothing And (myWeb.moConfig("RewriteRuleForProduct") <> "") Then
                                     strNewUrl = myWeb.moConfig("RewriteRuleForProduct").ToString() & strNewUrl
                                     strOldurl = myWeb.moConfig("RewriteRuleForProduct").ToString() & strOldurl
+                                Else
+                                    If moRequest("pageOldUrl") IsNot Nothing Then
+                                        oURL = moRequest("pageOldUrl").ToString()
+                                        Dim strarr() As String
+                                        strarr = oURL.Split("?"c)
+                                        oURL = strarr(0)
+                                    End If
+                                    Dim url As String = myWeb.GetContentUrl(pgid)
+                                    strOldurl = oURL & url & strOldurl
+                                    strNewUrl = oURL & url & strNewUrl
                                 End If
 
 
