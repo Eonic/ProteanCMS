eed062d5b46f622549bf5db9872ccc4aee3395fd
Sonali Sonwane <sonali.sonwane@infysion.com>
2021-03-19 20:30:56 +0530

    #5404: page/product redirect changes.

--- Assemblies/ProteanCMS/CMS/Admin/AdminXforms.vb
+++ Assemblies/ProteanCMS/CMS/Admin/AdminXforms.vb
@@ -1740,17 +1740,17 @@ Partial Public Class Cms
                                     newUrl = newUrl.Replace(" ", "-")
                                 End If
                                 newUrl = strOldurl.Replace(cName, newUrl)
-                                Dim obj As Admin.Redirects = New Admin.Redirects()
+
 
 
 
                                 Select Case moRequest("redirectType")
                                     Case "301Redirect"
 
-                                        obj.CreateRedirect(redirectType, strOldurl, newUrl, "", pgid, isParentPage)
+                                        oAdminRedirect.CreateRedirect(redirectType, strOldurl, newUrl, "", pgid, isParentPage)
 
                                     Case "302Redirect"
-                                        obj.CreateRedirect(redirectType, strOldurl, newUrl, "", pgid, isParentPage)
+                                        oAdminRedirect.CreateRedirect(redirectType, strOldurl, newUrl, "", pgid, isParentPage)
 
                                     Case "404Redirect"
 
