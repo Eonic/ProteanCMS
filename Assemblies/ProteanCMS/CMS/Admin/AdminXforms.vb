3e7fa934dbb728e32efc701bf1eedf2e2e1ef3f4
Sonali Sonwane <sonali.sonwane@infysion.com>
2021-03-19 14:19:14 +0530

    #5404: product/page redirect changes

--- Assemblies/ProteanCMS/CMS/Admin/AdminXforms.vb
+++ Assemblies/ProteanCMS/CMS/Admin/AdminXforms.vb
@@ -1761,6 +1761,9 @@ Partial Public Class Cms
 
                                     Case "302Redirect"
                                         obj.CreateRedirect(redirectType, strOldurl, newUrl, "", pgid, isParentPage)
+
+                                    Case "404Redirect"
+
                                 End Select
 
                                 Else
@@ -2933,6 +2936,8 @@ Partial Public Class Cms
 
                                     Case "302Redirect"
                                         obj.CreateRedirect(redirectType, strOldurl, strNewUrl)
+
+                                    Case "404Redirect"
 
                                 End Select
 
