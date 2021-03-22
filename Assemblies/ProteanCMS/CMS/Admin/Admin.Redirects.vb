4b202c2318cbad7340e8bfee2e38f1aacb39ef49
Sonali Sonwane <sonali.sonwane@infysion.com>
2021-03-22 13:56:20 +0530

    #5404: page /product redirect changes after call

--- Assemblies/ProteanCMS/CMS/Admin/Admin.Redirects.vb
+++ Assemblies/ProteanCMS/CMS/Admin/Admin.Redirects.vb
@@ -28,7 +28,7 @@ Partial Public Class Cms
                 moDbHelper = myWeb.moDbHelper
             End Sub
 
-            Public Function CreateRedirect(ByRef redirectType As String, ByRef OldUrl As String, ByRef NewUrl As String, Optional ByVal hiddenOldUrl As String = "", Optional ByVal pageId As Integer = 0, Optional ByVal isParentPage As String = "") As String
+            Public Function CreateRedirect(ByRef redirectType As String, ByRef OldUrl As String, ByRef NewUrl As String, Optional ByVal hiddenOldUrl As String = "", Optional ByVal pageId As Long = 0, Optional ByVal isParentPage As String = "") As String
                 Try
 
                     Dim rewriteXml As New XmlDocument
@@ -111,7 +111,7 @@ Partial Public Class Cms
                                     End If
                     End If
                         Dim Result As String = "success"
-                    Return Result
+                        Return Result
 
                 Catch ex As Exception
                     RaiseEvent OnError(Me, New Protean.Tools.Errors.ErrorEventArgs(mcModuleName, "CreateRedirect", ex, ""))
@@ -413,14 +413,8 @@ Partial Public Class Cms
                 Return Result
             End Function
 
-            Public Function isParentPage(ByRef pageId As Integer) As Boolean
 
-                Dim Result As String = ""
-                If pageId > 0 Then
-                    Result = moDbHelper.isParent(pageId)
-                End If
-                Return Result
-            End Function
+            'Public Function redirectPage(ByRef oRedirectType As String, ByRef oOldUrl As String, ByRef oNewUrl As String, ByRef oRedirectChildPage As Boolean)
 
             Public Function isParentPage(ByRef pageId As Integer) As Boolean
 
