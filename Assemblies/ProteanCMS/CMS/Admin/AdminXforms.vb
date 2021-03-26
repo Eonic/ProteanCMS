e8d3dc20afdf8dd63601d02130615b8d139d2cac
Sonali Sonwane <sonali.sonwane@infysion.com>
2021-03-26 12:01:15 +0530

    #5404: changes fro redirection

--- Assemblies/ProteanCMS/CMS/Admin/AdminXforms.vb
+++ Assemblies/ProteanCMS/CMS/Admin/AdminXforms.vb
@@ -2851,16 +2851,6 @@ Partial Public Class Cms
                     ' Additional Processing : Post Build
                     Me.xFrmEditContentPostBuildProcessing(cContentSchemaName)
 
-
-                    If (cModuleType = "FormattedText") Then
-                        If moXformElmt.SelectSingleNode("model/instance/tblContent/cContentXmlBrief/Content/@mobileview") Is Nothing Then
-                            Dim viewElmnt As XmlElement = moXformElmt.SelectSingleNode("model/instance/tblContent/cContentXmlBrief/Content")
-                            viewElmnt.SetAttribute("mobileview", "true")
-                            moXformElmt.SelectSingleNode("model/instance/tblContent/cContentXmlBrief").AppendChild(viewElmnt)
-
-                        End If
-                    End If
-
                     If MyBase.isSubmitted Then
 
                         ' Additional Processing : Pre Submission 
