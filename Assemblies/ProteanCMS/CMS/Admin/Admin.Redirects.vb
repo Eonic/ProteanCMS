facca53aabd4228ab394ed793feb3d64988acb98
Sonali Sonwane <sonali.sonwane@infysion.com>
2021-04-06 13:47:36 +0530

    #5404: redirect review changes

--- Assemblies/ProteanCMS/CMS/Admin/Admin.Redirects.vb
+++ Assemblies/ProteanCMS/CMS/Admin/Admin.Redirects.vb
@@ -463,7 +463,8 @@ Partial Public Class Cms
 
                         Case "302Redirect"
                             CreateRedirect(oRedirectType, oOldUrl, oNewUrl, "", pgId, oRedirectChildPage)
-
+                        Case Else
+                            'do nothing
                     End Select
                 End If
                 Return result
