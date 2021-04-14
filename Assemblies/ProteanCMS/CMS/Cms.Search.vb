84602ae2f6e510bebd5287da04caad3cf7085b97
Shubhangi Patil <shubhangi.patil@infysion.com>
2021-04-14 12:17:13 +0530

    #5206:pagination change

--- Assemblies/ProteanCMS/CMS/Cms.Search.vb
+++ Assemblies/ProteanCMS/CMS/Cms.Search.vb
@@ -585,6 +585,7 @@ Partial Public Class Cms
                     resultsXML.SetAttribute("sortDir", myWeb.moRequest("sortDir"))
                     resultsXML.SetAttribute("TotalResult", totalResults)
                     resultsXML.SetAttribute("PerPageCount", PerPageCount)
+                    resultsXML.SetAttribute("lastPage", Math.Ceiling(totalResults / PerPageCount))
 
                     If myWeb.moConfig("SiteSearchIndexResultPaging") = "on" Then
                         resultsXML.SetAttribute("SiteSearchIndexResultPaging", "on")
