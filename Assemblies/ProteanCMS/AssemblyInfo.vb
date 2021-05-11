653d9c83e87c624c25a698371011e49ccc6db25d
Trevor Spink <trevors@office.eonic.co.uk>
2021-05-11 21:32:07 +0100

    Update to embeddedhtml form

--- Assemblies/ProteanCMS/AssemblyInfo.vb
+++ Assemblies/ProteanCMS/AssemblyInfo.vb
@@ -34,12 +34,12 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 
 
-<Assembly: AssemblyVersion("6.0.49.27")>
+<Assembly: AssemblyVersion("6.0.49.28")>
 
 <Assembly: AssemblyDelaySign(False)>
 <Assembly: AssemblyKeyFile("../../../eonic.snk")>
 <Assembly: AssemblyKeyName("")>
 
-<Assembly: AssemblyFileVersion("6.0.49.26")>
+<Assembly: AssemblyFileVersion("6.0.49.27")>
 <Assembly: NeutralResourcesLanguageAttribute("en")>
 
