74b1cf6a3ebde795f85f73a749f140c69a1300f0
Trevor Spink <trevors@office.eonic.co.uk>
2021-07-15 20:38:38 +0100

    Fix for purchasing subscriptions on WealdFoundation

--- Assemblies/ProteanCMS/AssemblyInfo.vb
+++ Assemblies/ProteanCMS/AssemblyInfo.vb
@@ -34,12 +34,12 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 
 
-<Assembly: AssemblyVersion("6.0.49.117")>
+<Assembly: AssemblyVersion("6.0.49.115")>
 
 <Assembly: AssemblyDelaySign(False)>
 <Assembly: AssemblyKeyFile("../../../eonic.snk")>
 <Assembly: AssemblyKeyName("")>
 
-<Assembly: AssemblyFileVersion("6.0.49.115")>
+<Assembly: AssemblyFileVersion("6.0.49.113")>
 <Assembly: NeutralResourcesLanguageAttribute("en")>
 
