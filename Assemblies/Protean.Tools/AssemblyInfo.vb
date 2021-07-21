37114a7408eceecae39c87b616fdd20680a5f5d9
Trevor Spink <trevors@office.eonic.co.uk>
2021-07-21 10:15:16 +0100

    Fixed a glitch with content move in advanced mode

--- Assemblies/Protean.Tools/AssemblyInfo.vb
+++ Assemblies/Protean.Tools/AssemblyInfo.vb
@@ -33,8 +33,8 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.0.49.62")>
-<Assembly: AssemblyFileVersion("6.0.49.62")>
+<Assembly: AssemblyVersion("6.0.49.64")>
+<Assembly: AssemblyFileVersion("6.0.49.64")>
 
 '<Assembly: System.Security.AllowPartiallyTrustedCallersAttribute()> 
 
