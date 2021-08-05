7bdadfe63148249ffbebd376bd341e0106cb9dce
Trevor Spink <trevors@office.eonic.co.uk>
2021-08-05 18:26:07 +0100

    Fix for 3431

--- Assemblies/Protean.Tools.Csharp/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools.Csharp/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.0.49.62")]
-[assembly: AssemblyFileVersion("6.0.49.61")]
+[assembly: AssemblyVersion("6.0.49.64")]
+[assembly: AssemblyFileVersion("6.0.49.63")]
 
--- Assemblies/Protean.Tools/AssemblyInfo.vb
+++ Assemblies/Protean.Tools/AssemblyInfo.vb
@@ -33,8 +33,8 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.0.49.83")>
-<Assembly: AssemblyFileVersion("6.0.49.83")>
+<Assembly: AssemblyVersion("6.0.49.86")>
+<Assembly: AssemblyFileVersion("6.0.49.86")>
 
 '<Assembly: System.Security.AllowPartiallyTrustedCallersAttribute()> 
 
