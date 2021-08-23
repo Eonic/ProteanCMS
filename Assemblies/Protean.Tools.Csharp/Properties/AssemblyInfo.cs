c9d61f80d435613108227799f704cf9836898bb4
Trevor Spink <trevors@office.eonic.co.uk>
2021-08-23 07:17:04 +0100

    update

--- Assemblies/Protean.Tools.Csharp/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools.Csharp/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.0.49.75")]
-[assembly: AssemblyFileVersion("6.0.49.74")]
+[assembly: AssemblyVersion("6.0.49.78")]
+[assembly: AssemblyFileVersion("6.0.49.77")]
 
--- Assemblies/ProteanCMS/AssemblyInfo.vb
+++ Assemblies/ProteanCMS/AssemblyInfo.vb
@@ -34,12 +34,12 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 
 
-<Assembly: AssemblyVersion("6.0.49.196")>
+<Assembly: AssemblyVersion("6.0.49.205")>
 
 <Assembly: AssemblyDelaySign(False)>
 <Assembly: AssemblyKeyFile("../../../eonic.snk")>
 <Assembly: AssemblyKeyName("")>
 
-<Assembly: AssemblyFileVersion("6.0.49.191")>
+<Assembly: AssemblyFileVersion("6.0.49.200")>
 <Assembly: NeutralResourcesLanguageAttribute("en")>
 
