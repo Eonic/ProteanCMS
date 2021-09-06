0ad0cf1283c803a87204ab607a470ae381c77fee
Trevor Spink <trevors@office.eonic.co.uk>
2021-09-06 13:42:57 +0100

    CSSBundle Notes

--- Assemblies/Protean.Tools.Csharp/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools.Csharp/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.0.49.78")]
-[assembly: AssemblyFileVersion("6.0.49.77")]
+[assembly: AssemblyVersion("6.0.49.81")]
+[assembly: AssemblyFileVersion("6.0.49.80")]
 
--- Assemblies/ProteanCMS/AssemblyInfo.vb
+++ Assemblies/ProteanCMS/AssemblyInfo.vb
@@ -34,12 +34,12 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 
 
-<Assembly: AssemblyVersion("6.0.49.205")>
+<Assembly: AssemblyVersion("6.0.49.209")>
 
 <Assembly: AssemblyDelaySign(False)>
 <Assembly: AssemblyKeyFile("../../../eonic.snk")>
 <Assembly: AssemblyKeyName("")>
 
-<Assembly: AssemblyFileVersion("6.0.49.200")>
+<Assembly: AssemblyFileVersion("6.0.49.204")>
 <Assembly: NeutralResourcesLanguageAttribute("en")>
 
