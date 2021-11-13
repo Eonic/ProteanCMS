6f9d27535cb0a89e436abcd5016a212aa667c880
Trevor Spink <trevors@office.eonic.co.uk>
2021-11-13 09:14:53 +0000

    Fix for 5880

--- Assemblies/Protean.Tools.Csharp/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools.Csharp/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.0.52.9")]
-[assembly: AssemblyFileVersion("6.0.52.9")]
+[assembly: AssemblyVersion("6.0.52.14")]
+[assembly: AssemblyFileVersion("6.0.52.13")]
 
