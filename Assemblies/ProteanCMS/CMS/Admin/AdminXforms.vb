ad48a31dec15b1cec7281cb60a6c9abc40f2cefe
Trevor Spink <trevors@office.eonic.co.uk>
2021-12-08 11:12:28 +0000

    Build

--- Assemblies/ProteanCMS/CMS/Admin/AdminXforms.vb
+++ Assemblies/ProteanCMS/CMS/Admin/AdminXforms.vb
@@ -2512,9 +2512,8 @@ Partial Public Class Cms
                     If AlternateFormName <> "" Then cXformName = AlternateFormName
                     If cModuleType <> "" Then cXformName = cXformName & "/" & cModuleType
                     Dim formPath As String = "/xforms/content/"
-
                     If goConfig("cssFramework") = "bs5" Then
-                        formPath = "/modules/" & cXformName & "/"
+                        formPath = "/modules/"
                     End If
 
                     If Not MyBase.load(formPath & cXformName & ".xml", myWeb.maCommonFolders) Then
