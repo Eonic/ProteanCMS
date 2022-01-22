d417f46b513df2d1de1ad662ca895c6a5f8cc3a4
Trevor Spink <trevor@eonic.com>
2022-01-22 12:16:13 +0000

    updates

---
+++ wwwroot/ewcommon/sqlUpdate/MSSQL2012/fn_spilt_string.sql
@@ -0,0 +1,21 @@
+Create FUNCTION [dbo].[fn_split_string]
+(
+    @string    nvarchar(max),
+    @delimiter nvarchar(max)
+)
+/*
+    The same as STRING_SPLIT for compatibility level < 130
+    https://docs.microsoft.com/en-us/sql/t-sql/functions/string-split-transact-sql?view=sql-server-ver15
+*/
+RETURNS TABLE AS RETURN
+(
+    SELECT 
+      --ROW_NUMBER ( ) over(order by (select 0))                            AS id     --  intuitive, but not correect
+        Split.a.value('let $n := . return count(../*[. << $n]) + 1', 'int') AS id
+      , Split.a.value('.', 'NVARCHAR(MAX)')                                 AS value
+    FROM
+    (
+        SELECT CAST('<X>'+REPLACE(@string, @delimiter, '</X><X>')+'</X>' AS XML) AS String
+    ) AS a
+    CROSS APPLY String.nodes('/X') AS Split(a)
+)
