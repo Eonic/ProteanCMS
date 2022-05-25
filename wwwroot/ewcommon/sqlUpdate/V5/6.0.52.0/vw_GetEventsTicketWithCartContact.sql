b9274dcde60643313f79d897dfa5235cc6d558d3
Trevor Spink <trevor@eonic.com>
2022-05-25 10:42:52 +0100

    Fixes to database upgrade
    Former-commit-id: e3e15939e52a34d78f1bf25c7ad740627cb86af3

--- wwwroot/ewcommon/sqlUpdate/V5/6.0.52.0/vw_GetEventsTicketWithCartContact.sql
+++ wwwroot/ewcommon/sqlUpdate/V5/6.0.52.0/vw_GetEventsTicketWithCartContact.sql
@@ -1,27 +1,3 @@
-CREATE VIEW dbo.vw_GetDistinctEvents AS  
-SELECT distinct [tblContentRelation].nContentParentId, [dbo].[tblContent].[nContentKey], [tblContent].cContentName,
-		[dbo].[tblContent].cContentSchemaName
-FROM [dbo].[tblContentRelation] INNER JOIN [dbo].[tblContent] ON [tblContentRelation].nContentParentId = [tblContent].nContentKey
-WHERE [dbo].[tblContent].[cContentSchemaName] = 'Event'
-
-GO
-
---To get distinct tickets
-CREATE VIEW dbo.vw_GetDistinctTickets AS  
-SELECT distinct [tblContentRelation].nContentParentId, [tblContentRelation].nContentChildId, [dbo].[tblContent].[nContentKey], [tblContent].cContentName,
-		[dbo].[tblContent].cContentSchemaName
-FROM [dbo].[tblContentRelation] LEFT OUTER JOIN [dbo].[tblContent] ON [tblContentRelation].nContentParentId = [tblContent].nContentKey
-WHERE [dbo].[tblContent].[cContentSchemaName] IN ('Ticket')
- 
- GO
---To get distinct events and tickets
-CREATE VIEW dbo.vw_GetDistinctEventsTickets AS 
-SELECT vw_GetDistinctEvents.nContentKey As 'EventKey', vw_GetDistinctEvents.nContentParentId, vw_GetDistinctEvents.cContentName AS 'EventName', vw_GetDistinctEvents.cContentSchemaName AS 'TypeE',
-vw_GetDistinctTickets.nContentKey AS 'TicketId', vw_GetDistinctTickets.cContentName AS 'TicketName', vw_GetDistinctTickets.cContentSchemaName AS 'TypeT'
-FROM vw_GetDistinctEvents INNER JOIN vw_GetDistinctTickets ON vw_GetDistinctEvents.nContentParentId = vw_GetDistinctTickets.nContentChildId
-
-GO
-
 --To get list of events/tickets with contact and price. This is for the carts with status 6, 9 and 17 (complete, shipped and inprogress)
 CREATE VIEW dbo.vw_GetEventsTicketWithCartContact AS 
 SELECT distinct Items.nCartOrderId AS 'CartId', vw_GetDistinctEventsTickets.EventName, vw_GetDistinctEventsTickets.TicketName, vw_GetDistinctEventsTickets.TicketId,
@@ -38,5 +14,3 @@ FROM
 WHERE
 		(Items.nParentId IS NULL OR Items.nParentId = 0) AND (dbo.tblCartContact.cContactType LIKE N'Billing Address')  
 		AND nCartStatus IN (6, 9, 17)
-
-		GO
