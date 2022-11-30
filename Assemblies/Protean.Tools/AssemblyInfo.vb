79d6378669f394c548fc6f755d834925471d7af2
Trevor Spink <trevor@eonic.com>
2022-11-30 15:52:02 +0000

    Updates
    Former-commit-id: d4221902267af7033c7f02d2dbdf9c88a904346f

--- Assemblies/Protean.Tools/AssemblyInfo.vb
+++ Assemblies/Protean.Tools/AssemblyInfo.vb
@@ -33,9 +33,9 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.0.56.11")>
-<Assembly: AssemblyFileVersion("6.0.56.11")>
+<Assembly: AssemblyVersion("6.0.56.12")>
+<Assembly: AssemblyFileVersion("6.0.56.12")>
 
 '<Assembly: System.Security.AllowPartiallyTrustedCallersAttribute()> 
 
-<assembly: AssemblyInformationalVersion("0.0.0.58")>
+<assembly: AssemblyInformationalVersion("0.0.0.59")>
