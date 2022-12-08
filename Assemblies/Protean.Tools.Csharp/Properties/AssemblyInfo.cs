ab5b639101fbb64c70d45307c509b6399b947822
Trevor Spink <trevor@eonic.com>
2022-12-08 18:19:29 +0000

    updates
    Former-commit-id: 81f8499f39ed1e6db5eae98573585a0766d0891f

--- Assemblies/Protean.Tools.Csharp/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools.Csharp/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.0.58.4")]
-[assembly: AssemblyFileVersion("6.0.58.4")]
+[assembly: AssemblyVersion("6.0.58.5")]
+[assembly: AssemblyFileVersion("6.0.58.5")]
 
 
-[assembly: AssemblyInformationalVersion("0.0.0.68")]
+[assembly: AssemblyInformationalVersion("0.0.0.69")]
--- Assemblies/ProteanCMS/AssemblyInfo.vb
+++ Assemblies/ProteanCMS/AssemblyInfo.vb
@@ -34,13 +34,13 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 
 
-<Assembly: AssemblyVersion("6.0.58.9")>
+<Assembly: AssemblyVersion("6.0.58.10")>
 
 <Assembly: AssemblyDelaySign(False)>
 <Assembly: AssemblyKeyFile("../../../eonic.snk")>
 <Assembly: AssemblyKeyName("")>
 
-<Assembly: AssemblyFileVersion("6.0.58.9")>
+<Assembly: AssemblyFileVersion("6.0.58.10")>
 <Assembly: NeutralResourcesLanguageAttribute("en")>
 
-<Assembly: AssemblyInformationalVersion("0.0.0.252")>
+<Assembly: AssemblyInformationalVersion("0.0.0.253")>
