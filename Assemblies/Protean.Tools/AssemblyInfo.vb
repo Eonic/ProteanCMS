f16f1f34f1375aa51e161c4ba2ea966c2e6a4a9b
Trevor Spink <trevor@eonic.com>
2023-02-06 17:15:06 +0000

    Various updates for Judopay
    Former-commit-id: 3392cd0e126d07b22fff727129648ee6490ab18f

--- Assemblies/Protean.Tools/AssemblyInfo.vb
+++ Assemblies/Protean.Tools/AssemblyInfo.vb
@@ -33,9 +33,9 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.0.60.40")>
-<Assembly: AssemblyFileVersion("6.0.60.40")>
+<Assembly: AssemblyVersion("6.0.60.41")>
+<Assembly: AssemblyFileVersion("6.0.60.41")>
 
 '<Assembly: System.Security.AllowPartiallyTrustedCallersAttribute()> 
 
-<assembly: AssemblyInformationalVersion("0.0.0.173")>
+<assembly: AssemblyInformationalVersion("0.0.0.174")>
