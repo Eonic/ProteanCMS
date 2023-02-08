e099892c537d05ab99df809b73838c8a75e91581
Trevor Spink <trevor@eonic.com>
2023-02-08 14:54:26 +0000

    Remove MySQL import features for now
    Former-commit-id: 74e35444e1dbf25442c29d3793a7e5d5baa7919b

--- Assemblies/ProteanCMS/AssemblyInfo.vb
+++ Assemblies/ProteanCMS/AssemblyInfo.vb
@@ -33,13 +33,13 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 
 
-<Assembly: AssemblyVersion("6.0.60.7")>
+<Assembly: AssemblyVersion("6.0.60.36")>
 
 <Assembly: AssemblyDelaySign(False)>
 <Assembly: AssemblyKeyFile("../../../eonic.snk")>
 <Assembly: AssemblyKeyName("")>
 
-<Assembly: AssemblyFileVersion("6.0.60.7")>
+<Assembly: AssemblyFileVersion("6.0.60.36")>
 <Assembly: NeutralResourcesLanguageAttribute("en")>
 
-<Assembly: AssemblyInformationalVersion("0.0.0.375")>
+<Assembly: AssemblyInformationalVersion("0.0.0.404")>
