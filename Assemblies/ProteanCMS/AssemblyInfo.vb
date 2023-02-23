0e178edfe68bc8561fb4156d650941d9016c1720
Trevor Spink <trevor@eonic.com>
2023-02-23 16:14:19 +0000

    fix to GiftMessage
    Former-commit-id: 45cb494f659dfd6770ab73061f48d4b43778cd91

--- Assemblies/ProteanCMS/AssemblyInfo.vb
+++ Assemblies/ProteanCMS/AssemblyInfo.vb
@@ -33,13 +33,13 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 
 
-<Assembly: AssemblyVersion("6.0.60.83")>
+<Assembly: AssemblyVersion("6.0.60.84")>
 
 <Assembly: AssemblyDelaySign(False)>
 <Assembly: AssemblyKeyFile("../../../eonic.snk")>
 <Assembly: AssemblyKeyName("")>
 
-<Assembly: AssemblyFileVersion("6.0.60.83")>
+<Assembly: AssemblyFileVersion("6.0.60.84")>
 <Assembly: NeutralResourcesLanguageAttribute("en")>
 
-<Assembly: AssemblyInformationalVersion("0.0.0.451")>
+<Assembly: AssemblyInformationalVersion("0.0.0.452")>
