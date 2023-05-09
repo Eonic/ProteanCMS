829c185da31a96ed2c28a2b41b85b3c247cb985e
Trevor Spink <trevor@eonic.digital>
2023-05-09 11:19:29 +0100

    GA4 changes
    Former-commit-id: 2ed85564320f480ad3783251d567a990cd4eee25

--- Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.0.60.73")]
-[assembly: AssemblyFileVersion("6.0.60.73")]
+[assembly: AssemblyVersion("6.0.60.74")]
+[assembly: AssemblyFileVersion("6.0.60.74")]
 
 
 [assembly: AssemblyInformationalVersion("0.0.0.112")]
