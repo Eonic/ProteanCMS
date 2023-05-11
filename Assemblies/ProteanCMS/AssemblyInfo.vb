e3d14d4df2a26ab82441ec4fd812d15a85f5fab5
Trevor Spink <trevor@eonic.digital>
2023-05-11 16:21:32 +0100

    Fixes for paging on getContentFromSelect
    Former-commit-id: 6c0cc41f16c5ffb5f912e89f8a4734571d308a72

--- Assemblies/ProteanCMS/AssemblyInfo.vb
+++ Assemblies/ProteanCMS/AssemblyInfo.vb
@@ -33,13 +33,13 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 
 
-<Assembly: AssemblyVersion("6.0.60.151")>
+<Assembly: AssemblyVersion("6.0.60.154")>
 
 <Assembly: AssemblyDelaySign(False)>
 <Assembly: AssemblyKeyFile("../../../eonic.snk")>
 <Assembly: AssemblyKeyName("")>
 
-<Assembly: AssemblyFileVersion("6.0.60.151")>
+<Assembly: AssemblyFileVersion("6.0.60.154")>
 <Assembly: NeutralResourcesLanguageAttribute("en")>
 
-<Assembly: AssemblyInformationalVersion("0.0.0.519")>
+<Assembly: AssemblyInformationalVersion("0.0.0.522")>
