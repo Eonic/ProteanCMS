ff1576c761541ebcb382f9a4d30d94af167b6549
Santosh Chandratre <santosh@infysion.com>
2023-05-26 10:11:40 +0530

    changes for hypen logic
    Former-commit-id: e44a6cfaa00e5a6bc672286bc945000ce8f1c0c4

--- Assemblies/ProteanCMS/CMS/Admin/AdminXforms.vb
+++ Assemblies/ProteanCMS/CMS/Admin/AdminXforms.vb
@@ -1499,15 +1499,13 @@ Partial Public Class Cms
 
                     'Add the page name if passed through
                     If cName <> "" Then
-
+                        If myWeb.moConfig("PageURLFormat") = "hyphens" Then
+                            cName = cName.Replace("-", " ")
+                        End If
                         cProcessInfo = MyBase.Instance.InnerXml
                         MyBase.Instance.SelectSingleNode("tblContentStructure/cStructName").InnerText = cName
                     End If
-                    Dim cpageName = MyBase.Instance.SelectSingleNode("tblContentStructure/cStructName").InnerText
-                    If cpageName.Contains("-") Then
-                        cpageName = cpageName.Replace("-", " ")
-                        MyBase.Instance.SelectSingleNode("tblContentStructure/cStructName").InnerText = cpageName
-                    End If
+
                     ' delete the status if we are editing the home page
 
                     If pgid = myWeb.moConfig("RootPageId") Then
