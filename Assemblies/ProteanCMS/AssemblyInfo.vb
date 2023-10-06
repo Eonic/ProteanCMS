5d339544152faeeee88c38be3d451f5815f0b076
Trevor Spink <trevor@eonic.digital>
2023-10-06 17:14:41 +0100

    Updates to dynamics CRM

--- Assemblies/ProteanCMS/AssemblyInfo.vb
+++ Assemblies/ProteanCMS/AssemblyInfo.vb
@@ -33,13 +33,13 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 
 
-<Assembly: AssemblyVersion("6.0.66.14")>
+<Assembly: AssemblyVersion("6.0.66.22")>
 
 <Assembly: AssemblyDelaySign(False)>
 <Assembly: AssemblyKeyFile("../../../eonic.snk")>
 <Assembly: AssemblyKeyName("")>
 
-<Assembly: AssemblyFileVersion("6.0.66.14")>
+<Assembly: AssemblyFileVersion("6.0.66.22")>
 <Assembly: NeutralResourcesLanguageAttribute("en")>
 
-<Assembly: AssemblyInformationalVersion("0.0.0.768")>
+<Assembly: AssemblyInformationalVersion("0.0.0.776")>
