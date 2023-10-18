3005bbebff2ad83330e88552d8f5bf39ad0cf9d3
Trevor Spink <trevor@eonic.digital>
2023-10-18 17:15:03 +0100

    Updates to Dynamics Integration and allowing for better overloads of account creation forms.
    Former-commit-id: 8e94f0826a4b6f122e37c99d8eec84a532441ea5

--- Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.0.66.12")]
-[assembly: AssemblyFileVersion("6.0.66.12")]
+[assembly: AssemblyVersion("6.0.66.15")]
+[assembly: AssemblyFileVersion("6.0.66.15")]
 
 
 [assembly: AssemblyInformationalVersion("0.0.0.112")]
