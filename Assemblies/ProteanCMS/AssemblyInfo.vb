e193d4c648f09c4f32e805c332584849fb1a76d7
Trevor Spink <trevor@eonic.digital>
2023-11-13 13:09:18 +0000

    updates
    Former-commit-id: 506eb114692f49a804d5b40cac604397fd2c7fb7

--- Assemblies/ProteanCMS/AssemblyInfo.vb
+++ Assemblies/ProteanCMS/AssemblyInfo.vb
@@ -33,13 +33,13 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 
 
-<Assembly: AssemblyVersion("6.0.68.16")>
+<Assembly: AssemblyVersion("6.0.68.21")>
 
 <Assembly: AssemblyDelaySign(False)>
 <Assembly: AssemblyKeyFile("../../../eonic.snk")>
 <Assembly: AssemblyKeyName("")>
 
-<Assembly: AssemblyFileVersion("6.0.68.16")>
+<Assembly: AssemblyFileVersion("6.0.68.21")>
 <Assembly: NeutralResourcesLanguageAttribute("en")>
 
-<Assembly: AssemblyInformationalVersion("0.0.0.891")>
+<Assembly: AssemblyInformationalVersion("0.0.0.896")>
