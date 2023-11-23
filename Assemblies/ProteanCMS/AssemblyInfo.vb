921f29a0e9ec7757005069a05ed9e92a6ce8e5db
Nita Dubal <nita.dubal@infysion.com>
2023-11-23 15:07:00 +0530

    commit updates
    Former-commit-id: 84d6266dc40e09be7b5b61b0ed7755f7b0eb0a34

--- Assemblies/ProteanCMS/AssemblyInfo.vb
+++ Assemblies/ProteanCMS/AssemblyInfo.vb
@@ -33,13 +33,13 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 
 
-<Assembly: AssemblyVersion("6.0.69.156")>
+<Assembly: AssemblyVersion("6.0.69.159")>
 
 <Assembly: AssemblyDelaySign(False)>
 <Assembly: AssemblyKeyFile("../../../eonic.snk")>
 <Assembly: AssemblyKeyName("")>
 
-<Assembly: AssemblyFileVersion("6.0.69.156")>
+<Assembly: AssemblyFileVersion("6.0.69.159")>
 <Assembly: NeutralResourcesLanguageAttribute("en")>
 
-<Assembly: AssemblyInformationalVersion("0.0.0.1063")>
+<Assembly: AssemblyInformationalVersion("0.0.0.1066")>
