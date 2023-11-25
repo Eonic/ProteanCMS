e3c574ff0acf5cbe33534325bebfccd09d0f24c3
Trevor Spink <trevor@eonic.digital>
2023-11-25 12:12:27 +0000

    Updates to make Vend integration more robust
    Former-commit-id: 4a30922c574c764cf6af28ad14d43ce4355cb2ce

--- Assemblies/ProteanCMS/AssemblyInfo.vb
+++ Assemblies/ProteanCMS/AssemblyInfo.vb
@@ -33,13 +33,13 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 
 
-<Assembly: AssemblyVersion("6.0.69.34")>
+<Assembly: AssemblyVersion("6.0.69.39")>
 
 <Assembly: AssemblyDelaySign(False)>
 <Assembly: AssemblyKeyFile("../../../eonic.snk")>
 <Assembly: AssemblyKeyName("")>
 
-<Assembly: AssemblyFileVersion("6.0.69.34")>
+<Assembly: AssemblyFileVersion("6.0.69.39")>
 <Assembly: NeutralResourcesLanguageAttribute("en")>
 
-<Assembly: AssemblyInformationalVersion("0.0.0.941")>
+<Assembly: AssemblyInformationalVersion("0.0.0.946")>
