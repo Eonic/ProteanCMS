e64c6156ac1027348a92bd36db39e4a475cfca25
Trevor Spink <trevor@eonic.digital>
2023-12-11 19:32:07 +0000

    It is all broken
    Former-commit-id: f54c1d29b6d689da132199d826d385b6e4ee9451

--- Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.0.69.285")]
-[assembly: AssemblyFileVersion("6.0.69.285")]
+[assembly: AssemblyVersion("6.0.69.289")]
+[assembly: AssemblyFileVersion("6.0.69.289")]
 
 
 [assembly: AssemblyInformationalVersion("0.0.0.112")]
