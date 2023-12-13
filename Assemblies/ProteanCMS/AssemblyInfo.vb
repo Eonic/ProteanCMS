9db2ccd2549b15ee6f9434aa75fbce43266d90fe
Trevor Spink <trevor@eonic.digital>
2023-12-13 22:15:57 +0000

    Updates including Paypal integration
    Former-commit-id: 7731d8147cae38837fa10bf75dee63cc6e033c3f

--- Assemblies/ProteanCMS/AssemblyInfo.vb
+++ Assemblies/ProteanCMS/AssemblyInfo.vb
@@ -33,13 +33,13 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 
 
-<Assembly: AssemblyVersion("6.0.69.109")>
+<Assembly: AssemblyVersion("6.0.69.114")>
 
 <Assembly: AssemblyDelaySign(False)>
 <Assembly: AssemblyKeyFile("../../../eonic.snk")>
 <Assembly: AssemblyKeyName("")>
 
-<Assembly: AssemblyFileVersion("6.0.69.109")>
+<Assembly: AssemblyFileVersion("6.0.69.114")>
 <Assembly: NeutralResourcesLanguageAttribute("en")>
 
-<Assembly: AssemblyInformationalVersion("0.0.0.1016")>
+<Assembly: AssemblyInformationalVersion("0.0.0.1021")>
