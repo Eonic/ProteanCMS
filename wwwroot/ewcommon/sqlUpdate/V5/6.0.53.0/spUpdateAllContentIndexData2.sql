c729e385910ee1855638df75728048059a6ef3d3
Santosh Chandratre <santosh@infysion.com>
2024-01-09 20:34:39 +0530

    reverting
    Former-commit-id: 36193a00b12de96735282f1bfc83ec3f640b53fd

---
+++ wwwroot/ewcommon/sqlUpdate/V5/6.0.53.0/spUpdateAllContentIndexData2.sql
@@ -0,0 +1,33 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[spUpdateAllContentIndexData] 
+AS
+BEGIN
+DECLARE
+@ID int,
+@Name nchar(20)
+
+delete from tblContentIndex
+
+DECLARE cursor_content CURSOR FOR
+SELECT nContentKey, cContentSchemaName FROM tblContent
+
+OPEN cursor_content
+FETCH NEXT FROM cursor_content INTO @ID, @Name
+PRINT('ID  ' + 'Schema Name             ' )
+WHILE @@FETCH_STATUS=0
+BEGIN
+	PRINT(STR(@ID)+'    '+ @Name)
+
+	EXEC [spUpdateContentIndexData]
+		@cContentSchemaName = @Name,
+		@contentKey = @ID
+
+	FETCH NEXT FROM cursor_content INTO @ID, @Name
+
+END
+CLOSE cursor_content
+DEALLOCATE cursor_content
+END
