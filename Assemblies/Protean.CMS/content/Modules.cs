2043922d8cf59982aa1d31b50fd8805eff9b1115
Santosh <santosh@infysion.com>
2024-05-13 19:18:57 +0530

    moved number.cs as suggested in code review
    Former-commit-id: 2142648e97377acad61bc5d57daad683ed351693

--- Assemblies/Protean.CMS/content/Modules.cs
+++ Assemblies/Protean.CMS/content/Modules.cs
@@ -819,15 +819,7 @@ where cl.nStructId = " + myWeb.mnPageId));
                     }
                 }
 
-                public double DistanceCalculate(double fromLatitude, double fromLongitude,
-                    double toLatitude, double toLongitude)
-                {
-                    double x = 69.1 * (toLatitude - fromLatitude);
-                    double y = 69.1 * (toLongitude - fromLongitude) * Math.Cos(fromLatitude / 57.3);
-
-                    // Convert to KM by multiplying 1.609344
-                    return (Math.Sqrt(x * x + y * y) * 1.609344);
-                }
+              
             }
 
 
