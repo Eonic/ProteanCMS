5f788d32e56f828a65f8b00239f68de742abe8d5
Trevor Spink <trevor@eonic.digital>
2024-05-23 13:04:41 +0100

    Update to 6.1 c#
    Former-commit-id: b38effa61b79cd80976b060c4d97ef4d69c4d476

--- Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.1.3.70")]
-[assembly: AssemblyFileVersion("6.1.3.70")]
+[assembly: AssemblyVersion("6.1.3.72")]
+[assembly: AssemblyFileVersion("6.1.3.72")]
 
 
 [assembly: AssemblyInformationalVersion("0.0.0.115")]
