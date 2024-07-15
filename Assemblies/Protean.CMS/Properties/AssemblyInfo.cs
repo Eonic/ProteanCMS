75c27098b361bddb12fc8c6fab5bc4cc17486b32
Trevor Spink <trevor@eonic.digital>
2024-07-15 20:18:25 +0100

    MERGE

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.1.18.4")]
-[assembly: AssemblyFileVersion("6.1.18.4")]
+[assembly: AssemblyVersion("6.1.18.5")]
+[assembly: AssemblyFileVersion("6.1.18.5")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.409")]
+[assembly: AssemblyInformationalVersion("0.0.0.410")]
