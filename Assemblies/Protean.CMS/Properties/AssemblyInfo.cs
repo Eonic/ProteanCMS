995c157012c0c0527689b77ef20d6873cd46d150
Trevor Spink <trevor@eonic.digital>
2024-08-01 17:21:29 +0100

    ProteanCMS 6.1.21

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.1.20.3")]
-[assembly: AssemblyFileVersion("6.1.20.3")]
+[assembly: AssemblyVersion("6.1.21.1")]
+[assembly: AssemblyFileVersion("6.1.21.1")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.442")]
+[assembly: AssemblyInformationalVersion("0.0.0.443")]
--- Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.1.20.2")]
-[assembly: AssemblyFileVersion("6.1.20.2")]
+[assembly: AssemblyVersion("6.1.21.1")]
+[assembly: AssemblyFileVersion("6.1.21.1")]
 
 
 [assembly: AssemblyInformationalVersion("0.0.0.115")]
