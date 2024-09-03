b9132e78d0114ebfe3591eea9688e84398298b61
Callum Newman <callum@eonic.digital>
2024-09-03 12:43:47 +0100

    Added articles imports and adjusted content of the bible helps module

--- Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.1.21.45")]
-[assembly: AssemblyFileVersion("6.1.21.45")]
+[assembly: AssemblyVersion("6.1.21.46")]
+[assembly: AssemblyFileVersion("6.1.21.46")]
 
 
 [assembly: AssemblyInformationalVersion("0.0.0.115")]
