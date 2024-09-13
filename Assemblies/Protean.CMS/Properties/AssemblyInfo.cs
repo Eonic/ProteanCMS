6ccc3303ab3f0fb7633addd19690ee25519b57ee
Trevor Spink <trevor@eonic.digital>
2024-09-13 15:33:28 +0100

    updates to fix activitylog error

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.1.21.142")]
-[assembly: AssemblyFileVersion("6.1.21.142")]
+[assembly: AssemblyVersion("6.1.21.143")]
+[assembly: AssemblyFileVersion("6.1.21.143")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.585")]
+[assembly: AssemblyInformationalVersion("0.0.0.586")]
--- Assemblies/Protean.CMS/core/Cms.DBHelper.cs
+++ Assemblies/Protean.CMS/core/Cms.DBHelper.cs
@@ -8764,6 +8764,9 @@ namespace Protean
                     valuesList.Add(artId.ToString());
                     valuesList.Add(SqlDate(DateTime.Now, true));
                     valuesList.Add(((int)loggedActivityType).ToString());
+                    if (activityDetail.Length >= 800) {
+                        activityDetail = "TRUNCATED:" + activityDetail.Substring(0, 785);
+                    }
                     valuesList.Add(SqlString(activityDetail));
                     valuesList.Add(SqlString(Conversions.ToString(Interaction.IIf(string.IsNullOrEmpty(sessionId), "Service_" + DateTime.Now.ToString(), sessionId))));
                     if (otherId > 0L)
