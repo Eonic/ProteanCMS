3cf7491d55a7fc7c405e08551fc3da2fc9592562
Trevor Spink <trevor@eonic.digital>
2024-09-16 21:35:39 +0100

    updates

--- Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.1.21.69")]
-[assembly: AssemblyFileVersion("6.1.21.69")]
+[assembly: AssemblyVersion("6.1.21.71")]
+[assembly: AssemblyFileVersion("6.1.21.71")]
 
 
 [assembly: AssemblyInformationalVersion("0.0.0.115")]
