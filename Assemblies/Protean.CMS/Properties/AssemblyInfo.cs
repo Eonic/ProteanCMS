a1ea623398c39059a491896fe18f8ee0eae6551d
Callum Newman <ballunew@gmail.com>
2024-11-01 09:38:02 +0000

    Updates

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.1.22.127")]
-[assembly: AssemblyFileVersion("6.1.22.127")]
+[assembly: AssemblyVersion("6.1.22.128")]
+[assembly: AssemblyFileVersion("6.1.22.128")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.765")]
+[assembly: AssemblyInformationalVersion("0.0.0.766")]
--- Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.1.22.88")]
-[assembly: AssemblyFileVersion("6.1.22.88")]
+[assembly: AssemblyVersion("6.1.22.90")]
+[assembly: AssemblyFileVersion("6.1.22.90")]
 
 
 [assembly: AssemblyInformationalVersion("0.0.0.115")]
