98c5459ef6b18ca1f61a298b478251a414e336a9
Trevor Spink <trevor@eonic.digital>
2024-11-08 17:14:52 +0000

    Fix for campaign Monitor

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.1.22.130")]
-[assembly: AssemblyFileVersion("6.1.22.130")]
+[assembly: AssemblyVersion("6.1.22.135")]
+[assembly: AssemblyFileVersion("6.1.22.135")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.768")]
+[assembly: AssemblyInformationalVersion("0.0.0.773")]
