af4e1b26def1620c5d6c94175a0300e62031c6d6
Trevor Spink <trevor@eonic.digital>
2024-11-27 11:13:47 +0000

    updates for tools 2 standard2

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.1.24.64")]
-[assembly: AssemblyFileVersion("6.1.24.64")]
+[assembly: AssemblyVersion("6.1.24.65")]
+[assembly: AssemblyFileVersion("6.1.24.65")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.862")]
+[assembly: AssemblyInformationalVersion("0.0.0.863")]
