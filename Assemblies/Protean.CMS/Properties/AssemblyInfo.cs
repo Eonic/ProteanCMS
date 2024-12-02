1554a5c370b37f905cda016fb7b0579953f28dd4
Trevor Spink <trevor@eonic.digital>
2024-12-02 15:43:34 +0000

    updates

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.1.24.76")]
-[assembly: AssemblyFileVersion("6.1.24.76")]
+[assembly: AssemblyVersion("6.1.24.80")]
+[assembly: AssemblyFileVersion("6.1.24.80")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.874")]
+[assembly: AssemblyInformationalVersion("0.0.0.878")]
