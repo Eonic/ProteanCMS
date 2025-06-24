4d49396ab3d6da869577083885eaaaeacf29d629
Sonali Sonwane <sonali.sonwane@infysion.com>
2025-06-24 15:11:19 +0530

    #9410: filter changes

--- Assemblies/Protean.CMS/providers/filters/PriceFilter.cs
+++ Assemblies/Protean.CMS/providers/filters/PriceFilter.cs
@@ -204,7 +204,7 @@ namespace Protean.Providers
 
                         // Dim sText As String = "From " + aWeb.moCart.mcCurrencySymbol + "" + oMinPrice.Value.Trim() + " to " + aWeb.moCart.mcCurrencySymbol + "" + oMaxPrice.Value.Trim()
                         string sText = "From " + aWeb.moCart.mcCurrencySymbol + "" + oMinPrice.Value.Trim() + " to " + aWeb.moCart.mcCurrencySymbol + "" + oMaxPrice.Value.Trim()/*"From " + oMinPrice.Value.Trim() + " to " + oMaxPrice.Value.Trim()*/;
-                        oXform.addSubmit(ref oFromGroup, "PriceFilter", sText, "PriceFilter", "btnCrossForPrice filter-applied", "fa-times");
+                        oXform.addSubmit(ref oFromGroup, "PriceFilter", sText, "PriceFilter", "remove-PriceFilter filter-applied", "fa-times");
 
                     }
                     oXform.addInput(ref oPriceGroup, "", false, sCotrolDisplayName, "histogramSliderMainDivPrice histogramMain");
