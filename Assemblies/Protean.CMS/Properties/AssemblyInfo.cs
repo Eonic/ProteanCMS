80524c5b49473e5a9642874b88c2668126cf22a5
Victoria Jenkins <victoriaj@office.eonic.co.uk>
2025-09-23 19:01:37 +0100

    initial setup

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -35,10 +35,10 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("6.1.47.47")]
-[assembly: AssemblyFileVersion("6.1.47.47")]
+[assembly: AssemblyVersion("6.1.47.48")]
+[assembly: AssemblyFileVersion("6.1.47.48")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.1480")]
+[assembly: AssemblyInformationalVersion("0.0.0.1481")]
 
 
