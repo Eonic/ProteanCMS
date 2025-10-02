d67e8e730993edb0acdfeb54a2e8c7290621479d
Trevor M. Spink <trevor@eonic.digital>
2025-10-02 11:26:42 +0100

    updates

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -35,10 +35,10 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("6.1.47.73")]
-[assembly: AssemblyFileVersion("6.1.47.73")]
+[assembly: AssemblyVersion("6.1.47.84")]
+[assembly: AssemblyFileVersion("6.1.47.84")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.1506")]
+[assembly: AssemblyInformationalVersion("0.0.0.1517")]
 
 
