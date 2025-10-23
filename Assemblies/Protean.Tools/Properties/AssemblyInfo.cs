de676ba5a78aad78bad8fbb0d6b170c510eb9805
Victoria Jenkins <victoriaj@office.eonic.co.uk>
2025-10-23 10:02:24 +0100

    header CTA styling

--- Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("6.1.47.63")]
-[assembly: AssemblyFileVersion("6.1.47.63")]
+[assembly: AssemblyVersion("6.1.47.39")]
+[assembly: AssemblyFileVersion("6.1.47.39")]
 
 
 [assembly: AssemblyInformationalVersion("0.0.0.115")]
