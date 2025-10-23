c01dbc9a949b424d8a62d816be48b8909c6a0a49
Victoria Jenkins <victoriaj@office.eonic.co.uk>
2025-10-23 14:51:45 +0100

    merge

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -35,10 +35,10 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("6.1.47.169")]
-[assembly: AssemblyFileVersion("6.1.47.169")]
+[assembly: AssemblyVersion("6.1.47.171")]
+[assembly: AssemblyFileVersion("6.1.47.171")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.1602")]
+[assembly: AssemblyInformationalVersion("0.0.0.1604")]
 
 
