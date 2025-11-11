62fb1ad8dda761397dc9d1a2f308e7ff34b62064
Trevor M. Spink <trevor@eonic.digital>
2025-11-11 16:30:23 +0000

    updates

--- Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("6.1.51.2")]
-[assembly: AssemblyFileVersion("6.1.51.2")]
+[assembly: AssemblyVersion("6.1.51.3")]
+[assembly: AssemblyFileVersion("6.1.51.3")]
 
 
 [assembly: AssemblyInformationalVersion("0.0.0.115")]
