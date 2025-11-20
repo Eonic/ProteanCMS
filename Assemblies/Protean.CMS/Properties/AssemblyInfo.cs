0bf67c8fe40294695a0580d4497c00e1b88e9e50
Victoria Jenkins <victoriaj@office.eonic.co.uk>
2025-11-20 09:27:34 +0000

    header restyling

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -35,10 +35,10 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("6.1.53.46")]
-[assembly: AssemblyFileVersion("6.1.53.46")]
+[assembly: AssemblyVersion("6.1.53.48")]
+[assembly: AssemblyFileVersion("6.1.53.48")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.1738")]
+[assembly: AssemblyInformationalVersion("0.0.0.1740")]
 
 
