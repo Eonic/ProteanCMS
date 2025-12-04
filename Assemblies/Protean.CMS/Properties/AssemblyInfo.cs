f71968b6c820cf550899987d9401a700aae66bd3
Victoria Jenkins <victoriaj@office.eonic.co.uk>
2025-12-04 22:01:37 +0000

    fixes

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -35,10 +35,10 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("6.1.53.76")]
-[assembly: AssemblyFileVersion("6.1.53.76")]
+[assembly: AssemblyVersion("6.1.53.77")]
+[assembly: AssemblyFileVersion("6.1.53.77")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.1768")]
+[assembly: AssemblyInformationalVersion("0.0.0.1769")]
 
 
