a9800d049a8e0138ad12a5865cd28a4d805f411b
Trevor M. Spink <trevor@eonic.digital>
2025-12-13 11:22:36 +0000

    updates to fix showDependant issues on forms

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -35,10 +35,10 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("6.1.53.126")]
-[assembly: AssemblyFileVersion("6.1.53.126")]
+[assembly: AssemblyVersion("6.1.53.130")]
+[assembly: AssemblyFileVersion("6.1.53.130")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.1818")]
+[assembly: AssemblyInformationalVersion("0.0.0.1822")]
 
 
