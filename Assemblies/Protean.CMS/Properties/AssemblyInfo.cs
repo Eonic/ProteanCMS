6def537f5bfdae61ea1b47e6865c1d849222f583
Victoria Jenkins <victoriaj@office.eonic.co.uk>
2026-01-14 11:24:08 +0000

    styling fixes for ptn-base

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -35,10 +35,10 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("6.1.53.116")]
-[assembly: AssemblyFileVersion("6.1.53.116")]
+[assembly: AssemblyVersion("6.1.53.118")]
+[assembly: AssemblyFileVersion("6.1.53.118")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.1808")]
+[assembly: AssemblyInformationalVersion("0.0.0.1810")]
 
 
