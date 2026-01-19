5d38c85503fb9dac636be878a11afe19e183e361
Trevor M. Spink <trevor@eonic.digital>
2026-01-19 21:40:48 +0000

    Updates to remove VB code from cms.cs

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -35,10 +35,10 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("6.1.53.163")]
-[assembly: AssemblyFileVersion("6.1.53.163")]
+[assembly: AssemblyVersion("6.1.53.166")]
+[assembly: AssemblyFileVersion("6.1.53.166")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.1855")]
+[assembly: AssemblyInformationalVersion("0.0.0.1858")]
 
 
