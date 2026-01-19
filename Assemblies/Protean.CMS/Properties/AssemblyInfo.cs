f6abbea096cbc0635dea59070652893357fd21a1
Trevor M. Spink <trevor@eonic.digital>
2026-01-19 20:05:51 +0000

    breaking up cart.cs into logical files

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -35,10 +35,10 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("6.1.53.162")]
-[assembly: AssemblyFileVersion("6.1.53.162")]
+[assembly: AssemblyVersion("6.1.53.163")]
+[assembly: AssemblyFileVersion("6.1.53.163")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.1854")]
+[assembly: AssemblyInformationalVersion("0.0.0.1855")]
 
 
