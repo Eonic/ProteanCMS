2f48d0e6a25d04d4bd6346384a52413e81d0f444
Trevor M. Spink <trevor@eonic.digital>
2026-02-11 09:30:55 +0000

    merge

--- Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.Tools/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("6.1.53.77")]
-[assembly: AssemblyFileVersion("6.1.53.77")]
+[assembly: AssemblyVersion("6.1.53.87")]
+[assembly: AssemblyFileVersion("6.1.53.86")]
 
 
 [assembly: AssemblyInformationalVersion("0.0.0.115")]
