aa3853cd5cfefe84ccdf5d8f9e60587dcfc76a62
Victoria Jenkins <victoriaj@office.eonic.co.uk>
2026-02-16 11:47:16 +0000

    initial styling

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -35,10 +35,10 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("6.1.53.121")]
-[assembly: AssemblyFileVersion("6.1.53.121")]
+[assembly: AssemblyVersion("6.1.53.122")]
+[assembly: AssemblyFileVersion("6.1.53.122")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.1813")]
+[assembly: AssemblyInformationalVersion("0.0.0.1814")]
 
 
