3f45398d30b5e6a05f71721e38f40c8f37dbd6de
Nita Dubal <nita.dubal@infysion.com>
2026-03-06 17:16:33 +0530

    commit staged- change fixed for gift message, delete product changes

--- Assemblies/Protean.CMS/core/dbhelper/utils/APILog.cs
+++ Assemblies/Protean.CMS/core/dbhelper/utils/APILog.cs
@@ -119,6 +119,10 @@ namespace Protean
                         {
                             if (isActive)
                             {
+                                if (!string.IsNullOrEmpty(apiLog.cPayLoad))
+                                {
+                                    apiLog.cPayLoad = apiLog.cPayLoad.Replace("'", "''");
+                                }
                                 sSql = String.Format("INSERT INTO [dbo].[tblAPILog] ([nUserId],[dRequestDateTime],[cRequestedUrl],[cMethodName],[cPayLoad],[cRequestType],[cSourceIP],[cUserAgent],[cResponseData],[cResponseType]) VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}')", apiLog.nUserId, apiLog.dRequestDateTime, apiLog.cRequestedUrl, apiLog.cMethodName, apiLog.cPayLoad, apiLog.cRequestType, apiLog.cSourceIP, apiLog.cUserAgent, apiLog.cResponseData, apiLog.cResponseType);
 
                                 nId = myDbh.GetIdInsertSql(sSql);
