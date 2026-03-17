18500c379f2b47f4280016ffac9bc4d09ed06eb6
Nita Dubal <nita.dubal@infysion.com>
2026-03-17 15:05:30 +0530

    commit staged - supplier veneue edit issue

--- Assemblies/Protean.CMS/admin/Admin.cs
+++ Assemblies/Protean.CMS/admin/Admin.cs
@@ -2405,7 +2405,7 @@ namespace Protean
                             {
 
                                 sAdminLayout = "EditUserContact";
-                                oPageDetail.AppendChild(moAdXfm.xFrmEditDirectoryContact((long)Convert.ToInt16("0" + myWeb.moRequest["parid"]), Convert.ToInt16("0" + myWeb.moRequest["id"])));
+                                oPageDetail.AppendChild(moAdXfm.xFrmEditDirectoryContact((long)Convert.ToInt32("0" + myWeb.moRequest["parid"]), Convert.ToInt32("0" + myWeb.moRequest["id"])));
                                 if (moAdXfm.valid)
                                 {
                                     oPageDetail.RemoveAll();
