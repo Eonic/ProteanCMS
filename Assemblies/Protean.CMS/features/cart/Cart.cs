82310c5e0a4b593990e18419ec98adf0127de7f2
Sonali Sonwane <sonali.sonwane@infysion.com>
2026-03-24 14:23:07 +0530

    #10133

--- Assemblies/Protean.CMS/features/cart/Cart.cs
+++ Assemblies/Protean.CMS/features/cart/Cart.cs
@@ -3546,47 +3546,10 @@ namespace Protean
                         myWeb.moRequest.Cookies.Add(flagCookie);
 
                     }
-                    // Save cookiFirts consent  flag
+                     // Save cookiFirts consent  flag
                     if (mnCartId > 0)
                     {
-                        int isCookieConsentEnabled = 0; // default = disabled
-
-                        var request = HttpContext.Current?.Request;
-
-                        if (request != null)
-                        {
-                            HttpCookie consentCookie = myWeb.moRequest.Cookies["cookiefirst-consent"];
-                            HttpCookie cookieId = request.Cookies["cookiefirst-id"];
-
-                            if (consentCookie != null && !string.IsNullOrWhiteSpace(consentCookie.Value))
-                            {
-                                string cookieValue = HttpUtility.UrlDecode(consentCookie.Value);
-
-                                try
-                                {
-                                    JObject consentJson = JObject.Parse(cookieValue);
-
-                                    bool preferences = consentJson.Value<bool?>("preferences") == true;
-                                    bool statistics = consentJson.Value<bool?>("statistics") == true;
-                                    bool advertising = consentJson.Value<bool?>("advertising") == true;
-
-
-                                    if (preferences || statistics || advertising)
-                                    {
-                                        isCookieConsentEnabled = 1;
-                                    }
-                                }
-                                catch
-                                {
-                                    isCookieConsentEnabled = 0;
-                                }
-                            }
-
-
-                            string sSqlupdate = "UPDATE tblCartOrder SET isCookieConsentEnabled = " + isCookieConsentEnabled + " WHERE nCartOrderKey = " + mnCartId;
-
-                            moDBHelper.ExeProcessSql(sSqlupdate);
-                        }
+                        saveCookiesConsent();
 
                     }
                 }
@@ -7460,6 +7423,48 @@ namespace Protean
                         result = "Error: " + ex.Message;
                     }
                     return result;
+                }
+            }
+
+            public void saveCookiesConsent()
+            {
+                int isCookieConsentEnabled = 0; // default = disabled
+
+                var request = HttpContext.Current?.Request;
+
+                if (request != null)
+                {
+                    HttpCookie consentCookie = myWeb.moRequest.Cookies["cookiefirst-consent"];
+                    HttpCookie cookieId = request.Cookies["cookiefirst-id"];
+
+                    if (consentCookie != null && !string.IsNullOrWhiteSpace(consentCookie.Value))
+                    {
+                        string cookieValue = HttpUtility.UrlDecode(consentCookie.Value);
+
+                        try
+                        {
+                            JObject consentJson = JObject.Parse(cookieValue);
+
+                            bool preferences = consentJson.Value<bool?>("preferences") == true;
+                            bool statistics = consentJson.Value<bool?>("statistics") == true;
+                            bool advertising = consentJson.Value<bool?>("advertising") == true;
+
+
+                            if (preferences || statistics || advertising)
+                            {
+                                isCookieConsentEnabled = 1;
+                            }
+                        }
+                        catch
+                        {
+                            isCookieConsentEnabled = 0;
+                        }
+                    }
+
+
+                    string sSqlupdate = "UPDATE tblCartOrder SET isCookieConsentEnabled = " + isCookieConsentEnabled + " WHERE nCartOrderKey = " + mnCartId;
+
+                    moDBHelper.ExeProcessSql(sSqlupdate);
                 }
             }
 
