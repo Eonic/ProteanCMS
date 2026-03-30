0fcade577150ff9cf05eeb33f3bab90c624fcb95
Santosh Chandratre <santosh@infysion.com>
2026-03-30 22:45:16 +0100

    changes removed unnecessary

--- Assemblies/Protean.CMS/features/cart/Cart-invoice.cs
+++ Assemblies/Protean.CMS/features/cart/Cart-invoice.cs
@@ -353,6 +353,8 @@ namespace Protean
                 {
                     string PayableType = oCartElmt.GetAttribute("payableType");
 
+                  
+
                     // Add processing for deposits.
                     switch (PayableType ?? "")
                     {
@@ -445,7 +447,7 @@ namespace Protean
                 }
             }
 
-            public virtual void purchaseActions(XmlElement oCartElmt, bool bRenderScriptOnly = false)
+            public virtual void purchaseActions(XmlElement oCartElmt)
             {
                 myWeb.PerfMon.Log("Cart", "purchaseActions");
                 // Dim sMessageResponse As String
@@ -472,10 +474,10 @@ namespace Protean
 
                         if (passCMS == "true")
                         {
-                            args = new object[3];
+                            args = new object[2];
                             args[0] = myWeb;
                             args[1] = oCartElmt;
-                            args[2] = bRenderScriptOnly;
+                           
                         }
                         //else if (bRenderScriptOnly != null)
                         //{
@@ -541,18 +543,18 @@ namespace Protean
                                 var o = Activator.CreateInstance(calledType);
 
                                 var args = new object[2];
-                                if (bRenderScriptOnly == true)
-                                {
-                                    args = new object[3];
+                                //if (bRenderScriptOnly == true)
+                                //{
+                                //    args = new object[3];
+                                //    args[0] = myWeb;
+                                //    args[1] = ocNode;
+                                //    args[2] = bRenderScriptOnly;
+                                //}
+                                //else
+                                //{
                                     args[0] = myWeb;
                                     args[1] = ocNode;
-                                    args[2] = bRenderScriptOnly;
-                                }
-                                else
-                                {
-                                    args[0] = myWeb;
-                                    args[1] = ocNode;
-                                }
+                                //}
 
                                 calledType.InvokeMember(methodName, BindingFlags.InvokeMethod, null, o, args);
                             }
