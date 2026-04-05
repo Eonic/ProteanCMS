0377fb0df9750a988b01ea20494922a98539afcf
Trevor M. Spink <trevor@eonic.digital>
2026-04-05 22:04:44 +0100

    updates to ajax cart

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -35,10 +35,10 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("6.1.53.384")]
-[assembly: AssemblyFileVersion("6.1.53.384")]
+[assembly: AssemblyVersion("6.1.53.385")]
+[assembly: AssemblyFileVersion("6.1.53.385")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.2076")]
+[assembly: AssemblyInformationalVersion("0.0.0.2077")]
 
 
