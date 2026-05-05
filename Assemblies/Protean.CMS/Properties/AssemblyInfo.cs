3d8ea158b312d736eea53bea801fe30d00100940
Trevor M. Spink <trevor@eonic.digital>
2026-05-05 14:43:02 +0100

    Improvements to getvalidshippingoptions

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -35,10 +35,10 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("6.1.53.521")]
-[assembly: AssemblyFileVersion("6.1.53.521")]
+[assembly: AssemblyVersion("6.1.53.522")]
+[assembly: AssemblyFileVersion("6.1.53.522")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.2213")]
+[assembly: AssemblyInformationalVersion("0.0.0.2214")]
 
 
