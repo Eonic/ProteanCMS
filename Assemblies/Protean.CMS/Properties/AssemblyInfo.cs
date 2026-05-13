69767e81a0ba58149914d3ed0a6276e30a7a5ed8
Trevor M. Spink <trevor@eonic.digital>
2026-05-13 12:44:41 +0100

    Fixes to Judopay

--- Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
+++ Assemblies/Protean.CMS/Properties/AssemblyInfo.cs
@@ -35,10 +35,10 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("6.1.53.561")]
-[assembly: AssemblyFileVersion("6.1.53.561")]
+[assembly: AssemblyVersion("6.1.53.565")]
+[assembly: AssemblyFileVersion("6.1.53.565")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.2253")]
+[assembly: AssemblyInformationalVersion("0.0.0.2257")]
 
 
