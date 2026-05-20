36b448810b9421c7ed553e4f1603d73156a47a18
Trevor M. Spink <trevor@eonic.digital>
2026-05-20 16:36:56 +0100

    Updates

--- Assemblies/Protean.CMS/tools/xmlTools.xsltExtensions.cs
+++ Assemblies/Protean.CMS/tools/xmlTools.xsltExtensions.cs
@@ -1875,6 +1875,10 @@ namespace Protean
 
                 try
                 {
+                    if (cVirtualPath == "") {
+                        cVirtualPath = awaitingImgPath;
+                    }
+
                     // Use cached JpegQuality property to avoid repeated config lookups
                     if (myWeb != null)
                     {
@@ -1931,6 +1935,19 @@ namespace Protean
                                 break;
                             }
                     }
+
+                    if (myWeb != null)
+                    {
+                        if (myWeb.bs5)
+                        {
+                            newFilepath = newFilepath.Replace("/ptn/", "/");
+                        }
+                        else
+                        {
+                            newFilepath = newFilepath.Replace("/ewcommon/", "/");
+                        }
+                    }
+
                     if (myWeb is null)
                     {
                         return newFilepath;
