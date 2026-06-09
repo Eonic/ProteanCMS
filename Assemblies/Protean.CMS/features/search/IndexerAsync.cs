e18a48e06e84a4c3f0fc3004e7ec7abd88b5a898
Trevor M. Spink <trevor@eonic.digital>
2026-06-09 09:23:13 +0100

    updates

--- Assemblies/Protean.CMS/features/search/IndexerAsync.cs
+++ Assemblies/Protean.CMS/features/search/IndexerAsync.cs
@@ -1121,7 +1121,7 @@ namespace Protean
                      cPageHtml = cPageHtml.Replace("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">", "").Replace(" xmlns=\"http://www.w3.org/1999/xhtml\"", "");
                      cPageHtml = cPageHtml.Replace("<!DOCTYPE html>", "");
 
-                        if (string.IsNullOrEmpty(cPageHtml))
+                    if (string.IsNullOrEmpty(cPageHtml))
                     {
                         // we have an error to handle
                         if (myWeb.msException == "")
