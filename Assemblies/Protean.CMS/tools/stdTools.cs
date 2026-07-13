9f61afa833c3062c0712ef06e87c3171a041b229
Trevor M. Spink <trevor@eonic.digital>
2026-07-13 15:46:44 +0100

    updates

--- Assemblies/Protean.CMS/tools/stdTools.cs
+++ Assemblies/Protean.CMS/tools/stdTools.cs
@@ -375,7 +375,6 @@ namespace Protean
 
                                         }
                                     }
-
                                 }
                             }
                             catch (Exception ex)
